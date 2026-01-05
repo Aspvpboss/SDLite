8a8c8dfdbe40c9834c3dedbcb6c3b615aa36433d
Ben <b.f.vaughan2026@gmail.com>
2026-01-05 10:01:53 -0500

    started creating SDK_Create_AudioHandler

--- lib_dev/include/SDK_audio.h
+++ lib_dev/include/SDK_audio.h
@@ -8,6 +8,26 @@ extern "C"{
 
 #include "common_libs.h"
 
+typedef struct{
+
+    MIX_Track *track;
+    SDL_PropertiesID track_prop;
+    float track_volume;
+    int16_t loops;
+
+} SDK_Track;
+
+
+
+typedef struct{
+
+    float master_volume;
+    MIX_Mixer *mixer;
+    SDK_Track *tracks;
+    uint16_t track_capacity;
+
+} SDK_Audio_Handler;
+
 
 
 #ifdef __cplusplus
